8e74947784c3071055e71a4f27f3ff53db8c42f4
Akshat <akshatmiglani.am@gmail.com>
2024-10-25 23:22:13 +0530

    package.json added and mp3 renamed according to required naming

---
+++ sms.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+
+const accountSid=processs.env.TWILIO_ACCOUNT_SID;
+const authToken=processs.env.TWILO_AUTH_TOKEN;
+
+const client=require('twilio')(accountSid,authToken);
+const sendSMS=async(body) => {
+    let msgOptions={
+        from:process.env.TWILIO_FROM_NUMBER,
+        to:process.env.TO_NUMBER,
+        body
+    }
+    try{
+        const message=await client.messages.create(msgOptions);
+        console.log(message);
+    }catch(error){
+        console.error(error);
+    }
+}
+
+sendSMS("Hello from Node.js App")
